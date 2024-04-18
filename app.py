3f4b59b82cbe35b8538769ec95eaf1be971963cd
agatoneq <agata.sobczyk444@wp.pl>
2024-04-18 23:49:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,80 @@
 from shinyswatch import theme
-from shiny import App, render, ui, reactive
-
+from shiny import App, Inputs, Outputs, Session, render, ui, reactive
 from palmerpenguins import load_penguins
 
 penguins = load_penguins()
-ddi =3
+number_of_itemsets = 0
+selected_i = 0
+selected_e = 0
 
 
 
+def custom_value_transform(value):
+    if value <= 1:
+        return value * 0.2
+    else:
+        return (value - 1) * 2 + 0.2
 
+slider_value=0
+
+def make_items():
+    return [
+        ui.accordion_panel(f"Section {letter}", f"Some narrative for section {letter}")
+        for letter in "ABCDE"
+    ]
+       
+        
 def sidebar_text():
     return (
-    "Info about your data:",
-    "Number of itemstes: ",
-     ui.output_text("2"),
-    "selected itemstes: ", 
-     ui.output_text("3"),
-    "Selected examples: ",
-    ui.output_text("txt"),
+        ui.accordion(ui.accordion_panel(f"Section letter", 
+                                        ui.card(
+        ui.card_header("Info"),
+        ui.p(
+            f"Number of itemstes: {number_of_itemsets}", ui.br(),
+            f"Selected itemstes: {selected_i}", ui.br(), 
+            f"Selected examples: {selected_e}", ui.br(),
+        full_screen=True,),
+    ui.input_action_button("expand_all_button1", "Expand all"),  
+    ui.input_action_button("collapse_all_button1", "Collapse all"),
+    ),
+                                        ), 
+                     
+                     
+                     
+                     id="acc_single", multiple=True),
     
     
-    "Find itemsets: ",
-    ui.input_slider("minsupp_slider", "Minimal support:", 0, 100, 50),  
-    ui.input_slider("max_itemsets_slider", "Max. number of itemsets:", 0, 100, 50),  
+    ui.card(
+        ui.card_header("Find itemsets"),
+        ui.p(ui.input_slider("minsupp_slider", "Minimal support:", 0, 100, 2, step=slider_value, post="%")),
+        ui.p(ui.input_slider("max_itemsets_slider", "Max. number of itemsets:", 10, 100000, 10000)),
+        full_screen=True,
+    ),
     
+    ui.card(   
+        ui.card_header("Filter itemsets"),
+        ui.input_text("containing_text", "Contains: "),
+        ui.input_numeric("min_items_number", "Min. number of items:", 10, min=1, max=100),
+        ui.input_numeric("max_items_number", "Max. number of items:", 10, min=1, max=100),
+        ),
     
-    "Filter itemsets",
-    "Contains: ",
-    "Min. number of items: ",
-    "Max. number of items: ",
+    ui.input_task_button("confirm_button", "Confirm"),
     )
     
     
 app_ui = ui.page_sidebar(
-    ui.sidebar(sidebar_text(), position="top"),  
+    ui.sidebar(sidebar_text(), position="top", width=320),  
     theme.darkly(),
-    #ui.sidebar(),
-    #ui.page_opts(title="Hello shinyswatch theme"),
-    
-#     "Main content",
-
-     # ui.h2("Palmer Penguins"),
-    
     ui.output_data_frame("penguins_df"),   
     title="Association Rule Induction",
 )
 
+
 def server(input, output, session):
-    pass
-    #@render.text
-    
     @render.data_frame  
     def penguins_df():
         return render.DataGrid(penguins)
-    
-    
-  #  @render.text
+
     def com(str):
         count = 0
         for w in str:
@@ -63,11 +82,16 @@ def server(input, output, session):
                 count += 1
         return count
     
-    @render.text
-    def txt():
-        text="subject,trial,condition,sample,Fp1,AF7,AF3,F1,F3,F5,F7,FT7,FC5,FC3,FC1,C1,C3,C5,T7,TP7,CP5,CP3,CP1,P1,P3,P5,P7,P9,PO7,PO3,O1,Iz,Oz,POz,Pz,CPz,Fpz,Fp2,AF8,AF4,AFz,Fz,F2,F4,F6,F8,FT8,FC6,FC4,FC2,FCz,Cz,C2,C4,C6,T8,TP8,CP6,CP4,CP2,P2,P4,P6,P8,P10,PO8,PO4,O2,VEOa,VEOb,HEOL,HEOR,Nose,TP10"
-        return com(text)+1
-        #return f"n*2 is {input.n() * 2}"
+    @render.text()
+    @reactive.event(input.expand_all_button)
+    def expand_button_onclick():
+        return f"{input.expand_all_button()}"
     
+    @render.text()
+    @reactive.event(input.collapse_all_button)
+    def collapse_button_onclick():
+        return f"{input.collapse_all_button()}"
+    
+    #@render.text
     
 app = App(app_ui, server)
