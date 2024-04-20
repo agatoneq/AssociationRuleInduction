77ff85d1d643544dd8f3d608d2fe4c139e708d0f
agatoneq <agata.sobczyk444@wp.pl>
2024-04-20 11:16:56 +0200

    foodmart.csv

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ number_of_itemsets = 0
 selected_i = 0
 selected_e = 0
 
+number_of_rules = 0
+selected_r = 0
 
+
+
+
+#UI
 
 def custom_value_transform(value):
     if value <= 1:
@@ -17,51 +23,81 @@ def custom_value_transform(value):
 
 slider_value=0
 
-def make_items():
+def make_panels():
     return [
-        ui.accordion_panel(f"Section {letter}", f"Some narrative for section {letter}")
-        for letter in "ABCDE"
+        ui.accordion_panel(
+            f"Frequent Itemsets",
+            ui.card(
+                ui.card_header("Info"),
+                ui.p(
+                    f"Number of itemstes: {number_of_itemsets}", ui.br(),
+                    f"Selected itemstes: {selected_i}", ui.br(), 
+                    f"Selected examples: {selected_e}", ui.br(),
+                    full_screen=True,),
+                ui.input_action_button("expand_all_button1_fi", "Expand all"),  
+                ui.input_action_button("collapse_all_button1_fi", "Collapse all"),),
+                        
+            ui.card(
+                ui.card_header("Find itemsets"),
+                ui.p(ui.input_slider("minsupp_slider_fi", "Minimal support:", 0.1, 100, 2, step=1, post="%")),
+                ui.p(ui.input_slider("max_itemsets_slider_fi", "Max. number of itemsets:", 10, 100000, 10000)),
+                full_screen=True,
+            ),
+            
+            ui.card(   
+                ui.card_header("Filter itemsets"),
+                ui.input_text("containing_text_fi", "Contains: "),
+                ui.input_numeric("min_items_number_fi", "Min. number of items:", 10, min=1, max=100),
+                ui.input_numeric("max_items_number_fi", "Max. number of items:", 10, min=1, max=100),
+                ),
+            
+            ui.input_task_button("confirm_button_fi", "Confirm"), 
+            ),
+        
+        ui.accordion_panel(
+            f"Association Rules",
+            ui.card(
+                ui.card_header("Info"),
+                ui.p(
+                    f"Number of rules: {number_of_rules}", ui.br(),
+                    f"Selected rules: {selected_r}", ui.br(), 
+                    full_screen=True,),),
+                        
+            ui.card(
+                ui.card_header("Find association rules"),
+                ui.p(ui.input_slider("minsupp_slider_ar", "Minimal support:", 0.1, 100, 2, step=5, post="%")),
+                ui.p(ui.input_slider("minconf_slider_ar", "Minimal confidence:", 0.1, 100, 2, step=1, post="%")),
+                ui.p(ui.input_slider("max_rules_slider_ar", "Max. number of rules:", 10, 100000, 10000)),
+                full_screen=True,
+            ),
+            
+            ui.card(   
+                ui.card_header("Filter by Antecedent"),
+                ui.input_text("containing_text_ar", "Contains: "),
+                ui.input_numeric("min_items_number_ar_ant", "Min. number of items:", 10, min=1, max=100),
+                ui.input_numeric("max_items_number_ar_ant", "Max. number of items:", 10, min=1, max=100),
+                ),
+            
+            ui.card(   
+                ui.card_header("Filter by Consequent"),
+                ui.input_text("containing_text_ar", "Contains: "),
+                ui.input_numeric("min_items_number_ar_con", "Min. number of items:", 10, min=1, max=100),
+                ui.input_numeric("max_items_number_ar_con", "Max. number of items:", 10, min=1, max=100),
+                ),
+            
+            ui.input_task_button("confirm_button_ar", "Confirm"), 
+            )
     ]
        
         
 def sidebar_text():
     return (
-        ui.accordion(ui.accordion_panel(f"Section letter", 
-                                        ui.card(
-        ui.card_header("Info"),
-        ui.p(
-            f"Number of itemstes: {number_of_itemsets}", ui.br(),
-            f"Selected itemstes: {selected_i}", ui.br(), 
-            f"Selected examples: {selected_e}", ui.br(),
-        full_screen=True,),
-    ui.input_action_button("expand_all_button1", "Expand all"),  
-    ui.input_action_button("collapse_all_button1", "Collapse all"),
-    ),
-                                        ), 
-                     
-                     
-                     
-                     id="acc_single", multiple=True),
-    
-    
-    ui.card(
-        ui.card_header("Find itemsets"),
-        ui.p(ui.input_slider("minsupp_slider", "Minimal support:", 0, 100, 2, step=slider_value, post="%")),
-        ui.p(ui.input_slider("max_itemsets_slider", "Max. number of itemsets:", 10, 100000, 10000)),
-        full_screen=True,
-    ),
-    
-    ui.card(   
-        ui.card_header("Filter itemsets"),
-        ui.input_text("containing_text", "Contains: "),
-        ui.input_numeric("min_items_number", "Min. number of items:", 10, min=1, max=100),
-        ui.input_numeric("max_items_number", "Max. number of items:", 10, min=1, max=100),
-        ),
-    
-    ui.input_task_button("confirm_button", "Confirm"),
+        ui.accordion(*make_panels(), id="acc_frequent_itemsets", multiple=True),
     )
     
-    
+
+
+#UI    
 app_ui = ui.page_sidebar(
     ui.sidebar(sidebar_text(), position="top", width=320),  
     theme.darkly(),
@@ -70,7 +106,16 @@ app_ui = ui.page_sidebar(
 )
 
 
+
+
+
+
+
+
+#SERVER
+
 def server(input, output, session):
+    @render.text()
     @render.data_frame  
     def penguins_df():
         return render.DataGrid(penguins)
