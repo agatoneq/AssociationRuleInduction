5786f4fe4d7fddfe3280572715e08c7b2a96e4f7
agatoneq <agata.sobczyk444@wp.pl>
2024-04-25 14:19:28 +0200

    filtrowanie

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from shinyswatch import theme
 from shiny.types import FileInfo
 import os
 import pandas as pd
-import numpy as np
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules    
     
@@ -55,55 +54,54 @@ def make_panels():
             #     full_screen=True,
             # ),
                         
-            ui.card(
-                ui.card_header("Find itemsets"),
-                ui.p(ui.input_slider("minsupp_slider_fi", "Minimal support:", 0.1, 100, 2, step=1, post="%")),
-                ui.p(ui.input_slider("max_itemsets_slider_fi", "Max. number of itemsets:", 10, 100000, 10000)),
-                full_screen=True,
-            ),
+            # ui.card(
+            #     ui.card_header("Find itemsets"),
+            #     ui.p(ui.input_slider("minsupp_slider_fi", "Minimal support:", 0.1, 100, 2, step=1, post="%")),
+            #     ui.p(ui.input_slider("max_itemsets_slider_fi", "Max. number of itemsets:", 10, 100000, 10000)),
+            #     full_screen=True,
+            # ),
             
             ui.card(   
                 ui.card_header("Filter itemsets"),
+                ui.input_slider("minsupp_slider_fi", "Minimal support:", 0.1, 100, 2, step=1, post="%"),
                 ui.input_text("containing_text_fi", "Contains: "),
-                ui.input_numeric("min_items_number_fi", "Min. number of items:", 10, min=1, max=100),
-                ui.input_numeric("max_items_number_fi", "Max. number of items:", 10, min=1, max=100),
+                ui.input_numeric("min_values_per_itemset", "Min. number of items:", 1, min=1, max=100),
+                ui.input_numeric("max_values_per_itemset", "Max. number of items:", 1, min=1, max=100),
                 full_screen=True,
             ),
-            
-            ui.input_task_button("confirm_button_fi", "Confirm"), 
         ),
         
         ui.accordion_panel(
             f"Association Rules",
-            ui.card(
-                ui.card_header("Info"),
-                ui.p(
-                    f"Number of rules: {number_of_rules}", ui.br(),
-                    f"Selected rules: {selected_r}", ui.br()),
-                full_screen=True,
-            ),
+            # ui.card(
+            #     ui.card_header("Info"),
+            #     ui.p(
+            #         f"Number of rules: {number_of_rules}", ui.br(),
+            #         f"Selected rules: {selected_r}", ui.br()),
+            #     full_screen=True,
+            # ),
                         
             ui.card(
                 ui.card_header("Find association rules"),
                 ui.p(ui.input_slider("minsupp_slider_ar", "Minimal support:", 0.1, 100, 2, step=5, post="%")),
                 ui.p(ui.input_slider("minconf_slider_ar", "Minimal confidence:", 0.1, 100, 2, step=1, post="%")),
-                ui.p(ui.input_slider("max_rules_slider_ar", "Max. number of rules:", 10, 100000, 10000)),
+                #ui.p(ui.input_slider("max_rules_slider_ar", "Max. number of rules:", 10, 100000, 10000)),
                 full_screen=True,
             ),
             
             ui.card(   
                 ui.card_header("Filter by Antecedent"),
                 ui.input_text("containing_text_ant_ar", "Contains: "),
-                ui.input_numeric("min_items_number_ar_ant", "Min. number of items:", 10, min=1, max=100),
-                ui.input_numeric("max_items_number_ar_ant", "Max. number of items:", 10, min=1, max=100),
+                ui.input_numeric("min_items_number_ar_ant", "Min. number of items:", 1, min=1, max=100),
+                ui.input_numeric("max_items_number_ar_ant", "Max. number of items:", 1, min=1),
                 full_screen=True,
             ),
             
             ui.card(   
                 ui.card_header("Filter by Consequent"),
                 ui.input_text("containing_text_con_ar", "Contains: "),
-                ui.input_numeric("min_items_number_ar_con", "Min. number of items:", 10, min=1, max=100),
-                ui.input_numeric("max_items_number_ar_con", "Max. number of items:", 10, min=1, max=100),
+                ui.input_numeric("min_items_number_ar_con", "Min. number of items:", 1, min=1, max=100),
+                ui.input_numeric("max_items_number_ar_con", "Max. number of items:", 1, min=1),
                 full_screen=True,
             ),
             
@@ -122,7 +120,7 @@ def sidebar_text():
 app_ui = ui.page_sidebar(
     ui.sidebar(sidebar_text(), width=320),  
     theme.darkly(),
-    ui.output_text("get_file_size"),
+    #ui.output_text("show_fr_itemsets2"),
     ui.output_text("find_frequent_itemsets"),
     title="Association Rule Induction",
     id = "main_view",
@@ -215,6 +213,12 @@ def server(input: Inputs, output: Outputs, session: Session):
             ui.update_checkbox("checkbox_fi", value=False)
             ui.update_checkbox("checkbox_ar", value=False)
 
+    @reactive.event(input.minsupp_slider_fi)
+    def change_supp_fi():
+        min_supp_fi = input.minsupp_slider_fi._value/100
+        return min_supp_fi   
+    
+    
     def find_fr_itemsets():
         pf = parsed_file()
         if pf.empty:
@@ -226,11 +230,21 @@ def server(input: Inputs, output: Outputs, session: Session):
         te = TransactionEncoder()
         te_ary = te.fit(dataset).transform(dataset)
         df = pd.DataFrame(te_ary, columns=te.columns_)
-        return apriori(df, min_support=0.0001, use_colnames=True)
+        return apriori(df, min_support=change_supp_fi(), use_colnames=True)
+        
 
     @render.data_frame
     def show_fr_itemsets():
-        return pd.DataFrame(find_fr_itemsets())
+        keyword = input.containing_text_fi().lower()
+        min_values_per_itemset = input.min_values_per_itemset()
+        max_values_per_itemset = input.max_values_per_itemset()
+        fr_itemsets = find_fr_itemsets()
+        if fr_itemsets.empty:
+            return pd.DataFrame()
+        else:
+            fr_itemsets_filtered = fr_itemsets[fr_itemsets.apply(lambda row: min_values_per_itemset <= len(row['itemsets']) <= max_values_per_itemset, axis=1)]
+            return fr_itemsets_filtered[fr_itemsets_filtered.astype(str).apply(lambda row: keyword in ''.join(row['itemsets']).lower(), axis=1)]
+
     
     @reactive.effect
     @reactive.event(input.checkbox_fi)
@@ -254,12 +268,53 @@ def server(input: Inputs, output: Outputs, session: Session):
             )
             ui.update_checkbox("checkbox_of", value=False)
             ui.update_checkbox("checkbox_ar", value=False)
-       
+   
+   
+    @reactive.event(input.minsupp_slider_ar)
+    def change_supp_ar():
+        min_supp_fi = input.minsupp_slider_ar._value/100
+        return min_supp_fi  
+    
+    @reactive.event(input.minconf_slider_ar)
+    def change_conf_ar():
+        min_supp_fi = input.minconf_slider_ar._value/100
+        return min_supp_fi  
+    
+        
     @render.data_frame
+    #@reactive.event(input.containing_text_ant_ar)
     def show_assoc_rules():
+        keyword_ant = input.containing_text_ant_ar().lower()
+        keyword_con = input.containing_text_con_ar().lower()
+        min_supp_ar = change_supp_ar()
+        min_conf_ar = change_conf_ar()
+        min_values_per_itemset_ant_ar = input.min_items_number_ar_ant()
+        max_values_per_itemset_ant_ar = input.max_items_number_ar_ant()
+        min_values_per_itemset_con_ar = input.min_items_number_ar_con()
+        max_values_per_itemset_con_ar = input.max_items_number_ar_con()
         freq_itemsets = find_fr_itemsets()
-        return pd.DataFrame(association_rules(freq_itemsets, metric="confidence", min_threshold=0.7))
-
+        if freq_itemsets.empty:
+            return pd.DataFrame()
+        else:
+            assoc_rules = association_rules(freq_itemsets, metric="confidence", min_threshold=min_conf_ar)
+            assoc_rules_filtered = assoc_rules[assoc_rules['support'] >= min_supp_ar]
+            # Filtrowanie po minimalnej i maksymalnej liczbie wartości w poprzednikach
+            assoc_rules_filtered_ant = assoc_rules_filtered[assoc_rules_filtered['antecedents'].apply(lambda x: min_values_per_itemset_ant_ar <= len(x) <= max_values_per_itemset_ant_ar)]
+            # Filtrowanie po minimalnej i maksymalnej liczbie wartości w następnikach
+            assoc_rules_filtered_con = assoc_rules_filtered_ant[assoc_rules_filtered_ant['consequents'].apply(lambda x: min_values_per_itemset_con_ar <= len(x) <= max_values_per_itemset_con_ar)]
+            # Filtrowanie po pełnych słowach kluczowych w poprzednikach i następnikach
+            assoc_rules_filtered_keywords = assoc_rules_filtered_con[
+                assoc_rules_filtered_con.astype(str).apply(lambda row: keyword_ant in ''.join(row['antecedents']).lower(), axis=1)]
+            assoc_rules_filtered_keywords = assoc_rules_filtered_keywords[
+                assoc_rules_filtered_keywords.astype(str).apply(lambda row: keyword_con in ''.join(row['consequents']).lower(), axis=1)]
+            
+           # ['consequents'].astype(str).apply(lambda x: keyword_con.lower() in ' '.join(x).lower())]
+            return pd.DataFrame(assoc_rules_filtered_keywords)
+           
+        #    fr_itemsets_filtered = fr_itemsets[fr_itemsets.apply(lambda row: min_values_per_itemset <= len(row['itemsets']) <= max_values_per_itemset, axis=1)]
+        #     return fr_itemsets_filtered[fr_itemsets_filtered.astype(str).apply(lambda row: keyword in ' '.join(row['itemsets']).lower(), axis=1)]
+    
+            
     @reactive.effect
     @reactive.event(input.checkbox_ar)
     def _():
@@ -373,6 +428,6 @@ def server(input: Inputs, output: Outputs, session: Session):
         else:
             ui.remove_ui("#inserted_card_initinfo")                      
                            
-                  
+          
                   
 app = App(app_ui, server)
